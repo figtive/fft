e113afb3fcebafae8a4b06911ad8b63a6b03695a
Ilman <muhamad.ilman@ui.ac.id>
2019-12-04 19:24:28 +0700

    fft compression in octave

---
+++ FftCompression.m
@@ -0,0 +1,26 @@
+function [ a ] = FFTCompression()
+    pkg load image;
+
+    image = imread('image1', 'jpg');
+    figure(1), imshow(image);
+
+    imageBw = rgb2gray(image);
+    [nx, ny] = size(imageBw);
+    figure(2), imshow(imageBw);
+
+    imageFft = fft2(imageBw);
+    fourierCoefficient = log(abs(fftshift(imageFft)) + 1);
+    fourierCoefficient = mat2gray(fourierCoefficient);
+    figure(3), imshow(fourierCoefficient);
+
+    threshold = 0.001 * max(max(abs(imageFft)));
+    indisces = abs(imageFft) > threshold;
+    imageCompressed = imageFft .* indisces;
+    imageCompressedConverted = uint8(real(ifft2(imageCompressed)));
+    count = nx * ny - sum(sum(indisces));
+    percentage = 100 - count / (nx * ny) * 100;
+    figure(4), imshow(imageCompressedConverted);
+    title(num2str(percentage));
+
+    pkg unload image;
+endfunction
